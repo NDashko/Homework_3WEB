2234ea699fc81e2724f25c5ebfdd0cd3fe3931a9
Наталья Дашко <ndashko@mail.ru>
2023-07-19 20:42:48 +0300

    Добавлен файл 1.js

---
+++ 1.js
@@ -0,0 +1,3 @@
+
+let degrees_Celsius = Number.parseInt(prompt('Введите температуру в градусах Цельсия'));
+alert(`Цельсия: ${degrees_Celsius}, Фаренгейт: ${Math.round((9 / 5) * degrees_Celsius + 32)}`)
